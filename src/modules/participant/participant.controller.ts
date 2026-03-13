665698b5e1c99d6455926550e802adcf4355100d
surawitpakeepol <surawitpakeepol@gmail.com>
2026-03-13 21:42:50 +0700

    เเก้หน้าBackend API จากหัวข้อ default เป็นparticipant

--- src/modules/participant/participant.controller.ts
+++ src/modules/participant/participant.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ParticipantService } from './participant.service';
 import { CreateParticipantDto } from './dto/create-participant.dto';
 import { UpdateParticipantDto } from './dto/update-participant.dto';
 
+@ApiTags('participant')
 @Controller('participant')
 export class ParticipantController {
   constructor(private readonly participantService: ParticipantService) {}
